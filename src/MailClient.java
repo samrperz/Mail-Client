623f6bc09a7379faa1ac1bb099b72c977ba2b597
Samuel Perz <samrperz@gmail.com>
2021-03-24 20:37:54 -0400

    Finished adding image.  Done

--- src/MailClient.java
+++ src/MailClient.java
@@ -1,28 +1,20 @@
-/**
- * TODO:
- * 
- * Get the smtp server name from the email inputs
- * 
- * System.getProperty("user.dir");
- */
-
-
-
 import java.io.*;
+
 //import java.net.*;
 import java.awt.*;
 import java.awt.event.*;
 
 //import javax.imageio.stream.ImageInputStream;
-import java.awt.image.BufferedImage;
+//import java.awt.image.BufferedImage;
 
-import javax.imageio.ImageIO;
+//import javax.imageio.ImageIO;
 import javax.swing.*;
 //import javax.swing.border.Border;
-import javax.swing.border.Border;
+//import javax.swing.border.Border;
+import javax.swing.filechooser.FileSystemView;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+//import java.net.InetAddress;
+//import java.net.UnknownHostException;
 import java.io.File;
 import java.io.IOException;
 
@@ -33,11 +25,14 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimeBodyPart;
 //import javax.mail.*;
 //import javax.mail.Session;
 //import javax.mail.MimeMessage;
 //import javax.mail.internet.*;
 
+import java.util.ArrayList;
 import java.util.Properties;
 
 
@@ -57,36 +52,37 @@ public class MailClient extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	/* this is for the Authentication GUI */
+	//variables for authenticating and sending messages
 	private String email;
 	private String toEmail;
 	private String password;
-	
 	private String[] host;
 	private String smtpHost;
 	private String port = "465";
-	
-	
+	private ArrayList<File> files = new ArrayList<File>();
 	
 	private JLabel userLabel = new JLabel("Email: ");
-	private JTextField emailField = new JTextField("samrperz@gmail.com", 40);
+	private JTextField emailField = new JTextField("", 40);
 	
 	private JLabel passLabel = new JLabel("Password: ");
-	private JPasswordField passField = new JPasswordField("rustycat1", 40);
-	//passField.setEchoCar("*");
+	private JPasswordField passField = new JPasswordField("", 40);
 	
 	private JLabel failLabel = new JLabel("");
 	
 	private JButton btLogin = new JButton("Login");
 	
 	
-	
-	/* The stuff for the GUI. */	
 
-	private JLabel titLabel = new JLabel("Mail Client");
+	//for authentication GUI
+	private JPanel userPanel = new JPanel(new BorderLayout());
+	private JPanel passPanel = new JPanel(new BorderLayout());
+	private JPanel failPanel = new JPanel(new BorderLayout());
+	private JPanel loginPanel = new JPanel(new BorderLayout());
+	private JPanel authField = new JPanel(new BorderLayout());
 	
-	//private JLabel fromLabel = new JLabel("From:");
-	//private JTextField fromField = new JTextField("", 40);
+	
+	//for message  GUI
+	private JLabel titLabel = new JLabel("Mail Client");
 	
 	private JLabel ccLabel = new JLabel("CC: ");
 	private JTextField ccField = new JTextField("", 40);
@@ -97,7 +93,7 @@ public class MailClient extends JFrame {
 	private JLabel subjectLabel = new JLabel("Subject:");
 	private JTextField subjectField = new JTextField("", 40);
 	
-	private JLabel imgLabel = new JLabel("ImagePath: ");// C:/Users/samrp/Downloads/...
+	private JLabel imgLabel = new JLabel("ImagePath: ");
 	private JTextField imgField  = new JTextField("", 40);
 	
 	private JLabel messageLabel = new JLabel("Message:");
@@ -108,16 +104,6 @@ public class MailClient extends JFrame {
 	private JButton btQuit = new JButton("Quit");
 	private JButton btBrowse = new JButton("Add Image");
 	
-	
-	//for authentication GUI
-	private JPanel userPanel = new JPanel(new BorderLayout());
-	private JPanel passPanel = new JPanel(new BorderLayout());
-	private JPanel failPanel = new JPanel(new BorderLayout());
-	private JPanel loginPanel = new JPanel(new BorderLayout());
-	private JPanel authField = new JPanel(new BorderLayout());
-	
-	
-	//for messge GUI
 	private JPanel titPanel = new JPanel(new BorderLayout());
 	private JPanel ccPanel = new JPanel(new BorderLayout());
 	private JPanel toPanel = new JPanel(new BorderLayout());
@@ -126,32 +112,14 @@ public class MailClient extends JFrame {
 	private JPanel messagePanel = new JPanel(new BorderLayout());
 	private JPanel imgDisplayPanel = new JPanel(new BorderLayout());
 	private JPanel fieldPanel = new JPanel(new GridLayout(0, 1));
-	private JFrame frame = new JFrame("Mail Client");
-	
-	
-//	private String toAddr = "samrperz@gmail.com";
-//	private String fromAddr = "samrperz@gmail.com";
-//	private String[] host = toAddr.split("@", 2);
-	
-	
-	
+	private Panel buttonPanel = new Panel(new GridLayout(1, 0));
+	private JFrame frame = new JFrame("Mail Client");	
 
-	/** ------------added by me--------------
-	 *  addes support for the image file
-	 */
-	File file;
-	//BufferedImage img;
-	JLabel iconLabel = new JLabel("Attachments:");
 	
-	//public static final String imgPath = "/C:/Users/samrp/Downloads/cat.jpg";
-	private ImageIcon img;// = new ImageIcon(imgPath);
-	private JLabel imgIcon = new JLabel(img);
+	private JLabel iconLabel = new JLabel("Attachments:");
+	private JLabel imgIcon = new JLabel();
 
 
-	/**
-	 * Create a new MailClient window with fields for entering all the relevant
-	 * information (From, To, Subject, and message).
-	 */
 	@SuppressWarnings("deprecation")
 	public MailClient() {
 		super("Java Mailclient");
@@ -160,14 +128,13 @@ public class MailClient extends JFrame {
 		 * extra panel for holding all the child panels.
 		 */
 		/* auth panel*/
-		
 		userPanel.add(userLabel, BorderLayout.NORTH);
 		userPanel.add(emailField, BorderLayout.SOUTH);
 		
 		passPanel.add(passLabel, BorderLayout.NORTH);
 		passPanel.add(passField, BorderLayout.SOUTH);
 		
-		btLogin.addActionListener(new Authenticate());
+		btLogin.addActionListener(new AuthenticateListener());
 		loginPanel.add(btLogin);
 		
 		failPanel.add(failLabel);
@@ -202,23 +169,12 @@ public class MailClient extends JFrame {
 			toEmail = toField.getText();
 			System.out.println("Sending mail to " + toEmail);
 			
-			System.out.println(email + "    " + password);
-			//add check emails valid
-			// check message valid
-			//add cc to the mix
-			
-			//host = toAddr.split("@", 2);
-			
 			Properties props = System.getProperties();
 			props.put("mail.smtp.host", smtpHost);
 			props.put("mail.smtp.port", port);
 			props.put("mail.smtp.auth", "true");
 			props.put("mail.smtp.socketFactory.port", port);
-			//props.put("mail.smtp.transport.class", "com.sun.mail.smtp.SSLTransport");
-			props.put("mail.smtp.ssl.enable", "true");
-			//props.put("mail.smtp.starttls.enable", "true");
-			//props.put("mail.smtp.starttls.enable", props)
-			
+			props.put("mail.smtp.ssl.enable", "true");		
 			
 			
 			Session session = Session.getInstance(props, new javax.mail.Authenticator() {
@@ -228,19 +184,37 @@ public class MailClient extends JFrame {
 				}
 			});
 			
-			//System.out.println(session.getProperty("password"));
-			session.setDebug(true);
+			//session.setDebug(true);
 			try {
-				Message message= new MimeMessage(session);
+				MimeMessage message= new MimeMessage(session);
+				
+				
 				message.setFrom(new InternetAddress(email));
 				message.addRecipient(Message.RecipientType.TO, new InternetAddress(toField.getText()));
-				if(ccField.getText() != "") {
-					message.addRecipient(Message.RecipientType.CC, new InternetAddress(ccField.getText()));
-				}
+//				if(ccField.getText() != null) {
+//					message.addRecipient(Message.RecipientType.CC, new InternetAddress(ccField.getText()));
+//				}
 				
 				message.setSubject(subjectField.getText());
-				message.setText(messageText.getText());
-				System.out.println(message.getAllHeaders());
+				//message.setText(messageText.getText());
+				
+				MimeMultipart parts = new MimeMultipart();
+//				System.out.println(message.getAllHeaders());
+				MimeBodyPart text = new MimeBodyPart();
+				text.setText(messageText.getText());
+				parts.addBodyPart(text);
+				
+				for(int i = 0; i < files.size(); i++) {
+					try {
+						MimeBodyPart attach = new MimeBodyPart();
+						attach.attachFile(files.get(i));
+						parts.addBodyPart(attach);
+					} catch(IOException e) {
+						System.out.println("Error attaching file");
+					}
+				}
+				
+				message.setContent(parts);
 				
 				System.out.println(" - Message Ready: sending...");
 				Transport.send(message);
@@ -277,78 +251,25 @@ public class MailClient extends JFrame {
 
 	/* -------added by me---------- */
 	class AddImageListener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-			ccField.setText("hello");
-			imgField.setText("/C:/Users/samrp/Downloads/cat.jpg");
-//			String imgName = imgField.getText();
-//			File file = new File(imgName);
-//			if(file.exists()) {
-//				img = new ImageIcon(imgField.getText());// = new ImageIcon(imgPath);
-//				imgIcon.setLabelFor(img);
-//			}
-//			
-//			
-//			img = new ImageIcon(imgField.getText());// = new ImageIcon(imgPath);
-//			imgIcon.setLabelFor(img);// = new JLabel(img);
-			/**
-			 * fill out with code to recieve an image
-			 */
-			//JFileChooser browser = new JFileChooser(imgPath);
-//			try {
-//				String imgName = imgField.getText();
-//				File file = new File(imgName);
-//				if(file.exists()) {
-//					
-//				}
-//				
-//				//JLabel iconLabel = new JLabel("Attachments:");
-//				
-//				//String imgPath = "/C:/Users/samrp/Downloads/cat.jpg";
-//				img = new ImageIcon(imgName);
-//				imgIcon = new JLabel(img);
-//				
-//				imgDisplayPanel.add(iconLabel, BorderLayout.NORTH);
-//				imgDisplayPanel.add(imgIcon, BorderLayout.WEST);
-//				//try {
-//					//file = new File(imgField.getText());
-//					//file = new File("C:/Users/samrp/Downloads/cat.jpg");
-//					//img = ImageIO.read(file);
-//					
-//					
-//					
-//					//JFrame imgFrame = new JFrame("Images");
-//					//JPanel img1 = new JPanel(new BorderLayout());
-//					//JLabel icon = new JLabel(new ImageIcon(img));
-//					
-//					//img1.add(icon);
-//					//imgFrame.add(img1);
-//					//pack();
-//					//show();
-//					//
-//
-//				//} catch (IOException e1) {
-//					// TODO Auto-generated catch block
-//				//}
-//				
-//				
-//			} catch (Exception e) {
-//				System.out.println("Bad File")
-//			}
+		public void actionPerformed(ActionEvent e) {			
+			String imgName = imgField.getText();
+			File file = new File(imgName);
+			files.add(file);
 			
-		
+			Icon icon = FileSystemView.getFileSystemView().getSystemIcon(file);
 			
+			iconLabel = new JLabel(file.getPath(), icon, SwingConstants.LEFT);
+			
+			imgDisplayPanel.add(iconLabel);
+			
+			pack();
+			show();			
 		}
-		
 	}
 	
-	class Authenticate implements ActionListener{
+	class AuthenticateListener implements ActionListener{
 		public void actionPerformed(ActionEvent event) {
-			/**
-			 * start out with an authentication screen
-			 * Move the main body of the app over here
-			 * 
-			 * this stores username and password, and reveals the main app when send is hit
-			 */
+			imgField.setText("/C:/Users/samrp/Downloads/");
 			
 			email = emailField.getText();
 			password = passField.getText();
@@ -358,146 +279,62 @@ public class MailClient extends JFrame {
 			loginPanel.setVisible(false);
 			authField.setVisible(false);
 			
-			//String toAddr = "samrperz@gmail.com";
-			//fromAddr = "samrperz@gmail.com";
 			String address = email;
 			host = address.split("@", 2);
 			smtpHost = "smtp." + host[1];
-			System.out.println(smtpHost);
+			//System.out.println(smtpHost);
 			
-//			String smtpHost = "smpt." + host[1];
-//			String port = "465";
-			//ccField.setText(smtpHost);
+			imgDisplayPanel.add(iconLabel, BorderLayout.NORTH);
+
+			imgDisplayPanel.add(imgIcon, BorderLayout.WEST);
+
 			
+			titPanel.add(titLabel, BorderLayout.CENTER);
+
+			ccPanel.add(ccLabel, BorderLayout.NORTH);
+			ccPanel.add(ccField, BorderLayout.SOUTH);
+
+			toPanel.add(toLabel, BorderLayout.NORTH);
+			toPanel.add(toField, BorderLayout.SOUTH);
 			
-			//toField.setText("samrperz@gmail.com");
-			//subjectField.setText("test");
-			//messageText.setText("testing");
+			subjectPanel.add(subjectLabel, BorderLayout.NORTH);
+			subjectPanel.add(subjectField, BorderLayout.SOUTH);
 			
+			imgPanel.add(imgLabel, BorderLayout.NORTH);
+			imgPanel.add(imgField, BorderLayout.SOUTH);
 			
-			//if(check is login is good, then create the Client GUI){
-				//parse the smtp server from email
-			
-				imgDisplayPanel.add(iconLabel, BorderLayout.NORTH);
-				imgDisplayPanel.add(imgIcon, BorderLayout.WEST);
-	
-				titPanel.add(titLabel, BorderLayout.CENTER);
-	
-				ccPanel.add(ccLabel, BorderLayout.NORTH);
-				ccPanel.add(ccField, BorderLayout.SOUTH);
-	
-				toPanel.add(toLabel, BorderLayout.NORTH);
-				toPanel.add(toField, BorderLayout.SOUTH);
-				
-				subjectPanel.add(subjectLabel, BorderLayout.NORTH);
-				subjectPanel.add(subjectField, BorderLayout.SOUTH);
-				
-				imgPanel.add(imgLabel, BorderLayout.NORTH);
-				imgPanel.add(imgField, BorderLayout.SOUTH);
-				
-				messagePanel.add(messageLabel, BorderLayout.NORTH);
-				messagePanel.add(messageText, BorderLayout.SOUTH);
-				messagePanel.add(imgDisplayPanel);
-				
-				fieldPanel.add(titPanel);
-				fieldPanel.add(ccPanel);
-				fieldPanel.add(toPanel);
-				fieldPanel.add(subjectPanel);
-				fieldPanel.add(imgPanel);
-	
-				frame.add(fieldPanel);
-	
-				/*
-				 * Create a panel for the buttons and add listeners to the buttons.
-				 */
-				Panel buttonPanel = new Panel(new GridLayout(1, 0));
-				btSend.addActionListener(new SendListener());
-				btClear.addActionListener(new ClearListener());
-				btQuit.addActionListener(new QuitListener());
-				btBrowse.addActionListener(new AddImageListener());
-				
-				buttonPanel.add(btSend);
-				buttonPanel.add(btClear);
-				buttonPanel.add(btQuit);
-				buttonPanel.add(btBrowse);
-	
-				/* Add, pack, and show. */
-				add(fieldPanel, BorderLayout.NORTH);
-				add(messagePanel, BorderLayout.CENTER);
-				add(buttonPanel, BorderLayout.SOUTH);
-				pack();
-				show();
-			//}
+			messagePanel.add(messageLabel, BorderLayout.NORTH);
+			messagePanel.add(imgDisplayPanel, BorderLayout.CENTER);
+			messagePanel.add(messageText, BorderLayout.SOUTH);
 			
 			
-			//TODO reveal the rest of app
+			fieldPanel.add(titPanel);
+			fieldPanel.add(ccPanel);
+			fieldPanel.add(toPanel);
+			fieldPanel.add(subjectPanel);
+			fieldPanel.add(imgPanel);
+
+			frame.add(fieldPanel);
+
 			
+			btSend.addActionListener(new SendListener());
+			btClear.addActionListener(new ClearListener());
+			btQuit.addActionListener(new QuitListener());
+			btBrowse.addActionListener(new AddImageListener());
+			
+			buttonPanel.add(btSend);
+			buttonPanel.add(btClear);
+			buttonPanel.add(btQuit);
+			buttonPanel.add(btBrowse);
+
+			add(fieldPanel, BorderLayout.NORTH);
+			add(messagePanel, BorderLayout.CENTER);
+			add(buttonPanel, BorderLayout.SOUTH);
+			pack();
+			show();			
 		}
-		
-		
-		
 	}
 }
-
-
-
-
-
-
-
-/* Check that we have the local mailserver */
-//InetAddress addr;
-//try {
-//	addr = InetAddress.getLocalHost();
-//} catch (UnknownHostException e) {
-//	System.out.println("Can't determine host");
-//	return;
-//}
-//if ((serverField.getText()).equals("")) {
-//	System.out.println("Need name of local mailserver!");
-//	return;
-//}
-
-/* Check that we have the sender and recipient. */
-//if ((fromField.getText()).equals("")) {
-//	System.out.println("Need sender!");
-//	return;
-//}
-//if ((toField.getText()).equals("")) {
-//	System.out.println("Need recipient!");
-//	return;
-//}
-
-/* Create the message */
-//Message mailMessage = new Message(addr.getHostName(),
-//		toField.getText(), subjectField.getText(),
-//		messageText.getText());
-
-/*
- * Check that the message is valid, i.e., sender and recipient
- * addresses look ok.
- */
-//if (!mailMessage.isValid()) {
-//	return;
-//}
-
-/*
- * Create the envelope, open the connection and try to send the
- * message.
- */
-//try {
-//	Envelope envelope = new Envelope(mailMessage,
-//			"londo.ad.stetson.edu");
-//
-//	SMTPConnection connection = new SMTPConnection(envelope);
-//	connection.send(envelope);
-//	connection.close();
-//} catch (IOException error) {
-//	System.out.println("Sending failed: " + error);
-//	return;
-//}
-//System.out.println("Mail sent succesfully!");
-
 
 
 // https://stackoverflow.com/questions/13512612/browse-for-image-file-and-display-it-using-java-swing
